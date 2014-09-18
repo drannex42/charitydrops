915398eb3b5a479ff0fc4f21fd8ffc2684124b66
Macleod Sawyer <macleod@charitydrops.org>
2014-09-17 23:50:45 -0500

    Fixed shit, added shit, I honestly don't know what I fixed tbh
    Signed-off-by: Macleod Sawyer <macleod@charitydrops.org>

--- config/secrets.js
+++ config/secrets.js
@@ -38,85 +38,82 @@ module.exports = {
   },
 
   nyt: {
-    key: process.env.NYT_KEY || '9548be6f3a64163d23e1539f067fcabd:5:68537648'
+    key: process.env.NYT_KEY || 'hello'
   },
 
 
   facebook: {
-    clientID: process.env.FACEBOOK_ID || '754220301289665',
-    clientSecret: process.env.FACEBOOK_SECRET || '41860e58c256a3d7ad8267d3c1939a4a',
+    clientID: process.env.FACEBOOK_ID || '1513341008910186',
+    clientSecret: process.env.FACEBOOK_SECRET || '72cdc7b5cf7d90fd98f41e7495f3a199',
     callbackURL: '/auth/facebook/callback',
     passReqToCallback: true
   },
 
   instagram: {
-    clientID: process.env.INSTAGRAM_ID || '9f5c39ab236a48e0aec354acb77eee9b',
-    clientSecret: process.env.INSTAGRAM_SECRET || '5920619aafe842128673e793a1c40028',
+    clientID: process.env.INSTAGRAM_ID || 'helo',
+    clientSecret: process.env.INSTAGRAM_SECRET || 'hello',
     callbackURL: '/auth/instagram/callback',
     passReqToCallback: true
   },
 
   github: {
-    clientID: process.env.GITHUB_ID || 'cb448b1d4f0c743a1e36',
-    clientSecret: process.env.GITHUB_SECRET || '815aa4606f476444691c5f1c16b9c70da6714dc6',
+    clientID: process.env.GITHUB_ID || 'hello',
+    clientSecret: process.env.GITHUB_SECRET || 'hello',
     callbackURL: '/auth/github/callback',
     passReqToCallback: true
   },
 
   twitter: {
-    consumerKey: process.env.TWITTER_KEY || '6NNBDyJ2TavL407A3lWxPFKBI',
-    consumerSecret: process.env.TWITTER_SECRET  || 'ZHaYyK3DQCqv49Z9ofsYdqiUgeoICyh6uoBgFfu7OeYC7wTQKa',
+    consumerKey: process.env.TWITTER_KEY || 'hello',
+    consumerSecret: process.env.TWITTER_SECRET  || 'hello',
     callbackURL: '/auth/twitter/callback',
     passReqToCallback: true
   },
 
   google: {
-    clientID: process.env.GOOGLE_ID || '828110519058.apps.googleusercontent.com',
-    clientSecret: process.env.GOOGLE_SECRET || 'JdZsIaWhUFIchmC1a_IZzOHb',
+    clientID: process.env.GOOGLE_ID || 'hello',
+    clientSecret: process.env.GOOGLE_SECRET || 'hello',
     callbackURL: '/auth/google/callback',
     passReqToCallback: true
   },
 
   linkedin: {
-    clientID: process.env.LINKEDIN_ID || '77chexmowru601',
-    clientSecret: process.env.LINKEDIN_SECRET || 'szdC8lN2s2SuMSy8',
+    clientID: process.env.LINKEDIN_ID || 'hello',
+    clientSecret: process.env.LINKEDIN_SECRET || 'hello',
     callbackURL: '/auth/linkedin/callback',
     scope: ['r_fullprofile', 'r_emailaddress', 'r_network'],
     passReqToCallback: true
   },
 
-  steam: {
-    apiKey: process.env.STEAM_KEY || 'D1240DEF4D41D416FD291D0075B6ED3F'
-  },
 
   twilio: {
-    sid: process.env.TWILIO_SID || 'AC6f0edc4c47becc6d0a952536fc9a6025',
-    token: process.env.TWILIO_TOKEN || 'a67170ff7afa2df3f4c7d97cd240d0f3'
+    sid: process.env.TWILIO_SID || 'hello',
+    token: process.env.TWILIO_TOKEN || 'hello'
   },
 
   clockwork: {
-    apiKey: process.env.CLOCKWORK_KEY || '9ffb267f88df55762f74ba2f517a66dc8bedac5a'
+    apiKey: process.env.CLOCKWORK_KEY || 'hello'
   },
 
   stripe: {
-    apiKey: process.env.STRIPE_KEY || 'sk_test_BQokikJOvBiI2HlWgH4olfQ2'
+    apiKey: process.env.STRIPE_KEY || 'hello'
   },
 
   tumblr: {
-    consumerKey: process.env.TUMBLR_KEY || 'FaXbGf5gkhswzDqSMYI42QCPYoHsu5MIDciAhTyYjehotQpJvM',
-    consumerSecret: process.env.TUMBLR_SECRET || 'QpCTs5IMMCsCImwdvFiqyGtIZwowF5o3UXonjPoNp4HVtJAL4o',
+    consumerKey: process.env.TUMBLR_KEY || 'hello',
+    consumerSecret: process.env.TUMBLR_SECRET || 'hello',
     callbackURL: '/auth/tumblr/callback'
   },
 
   foursquare: {
-    clientId: process.env.FOURSQUARE_ID || '2STROLSFBMZLAHG3IBA141EM2HGRF0IRIBB4KXMOGA2EH3JG',
-    clientSecret: process.env.FOURSQUARE_SECRET || 'UAABFAWTIHIUFBL0PDC3TDMSXJF2GTGWLD3BES1QHXKAIYQB',
-    redirectUrl: process.env.FOURSQUARE_REDIRECT_URL || 'http://localhost:3000/auth/foursquare/callback'
+    clientId: process.env.FOURSQUARE_ID || 'hello',
+    clientSecret: process.env.FOURSQUARE_SECRET || 'hello',
+    redirectUrl: process.env.FOURSQUARE_REDIRECT_URL || '/auth/foursquare/callback'
   },
 
   venmo: {
-    clientId: process.env.VENMO_ID || '1688',
-    clientSecret: process.env.VENMO_SECRET || 'uQXtNBa6KVphDLAEx8suEush3scX8grs',
-    redirectUrl: process.env.VENMO_REDIRECT_URL || 'http://localhost:3000/auth/venmo/callback'
+    clientId: process.env.VENMO_ID || 'hello',
+    clientSecret: process.env.VENMO_SECRET || 'hello',
+    redirectUrl: process.env.VENMO_REDIRECT_URL || 'hello'
   }
 };
