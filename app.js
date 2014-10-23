fd84f671dad5eca6fcf7952b6823db822c25f8e9
Macleod Sawyer <macleod@charitydrops.org>
2014-10-23 08:36:08 -0500

    error pages maybe

--- app.js
+++ app.js
@@ -124,6 +124,20 @@ app.use(function(req, res, next) {
 });
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: week }));
 
+
+// Handle 404
+app.use(function(req, res) {
+  res.status(400);
+ res.render('404.jade', {title: '404: File Not Found'});
+});
+
+// Handle 500
+app.use(function(error, req, res, next) {
+  res.status(500);
+ res.render('500.jade', {title:'500: Internal Server Error', error: error});
+});
+
+
 /**
  * Main routes.
  */
