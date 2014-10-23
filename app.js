712938020d354e7fc03b7ef3ad24d51879b0938c
Macleod Sawyer <macleod@charitydrops.org>
2014-10-23 08:38:26 -0500

    Ayyyyeeeee

--- app.js
+++ app.js
@@ -128,13 +128,13 @@ app.use(express.static(path.join(__dirname, 'public'), { maxAge: week }));
 // Handle 404
 app.use(function(req, res) {
   res.status(400);
- res.render('404.jade', {title: '404: File Not Found'});
+ res.render('errors/404', {title: '404: File Not Found'});
 });
 
 // Handle 500
 app.use(function(error, req, res, next) {
   res.status(500);
- res.render('500.jade', {title:'500: Internal Server Error', error: error});
+ res.render('errors/500', {title:'500: Internal Server Error', error: error});
 });
 
 
